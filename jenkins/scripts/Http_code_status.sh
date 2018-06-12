ae8d75035ae8c00b7102cb8cdac51b6b2706f661
Rajiv Kumar 4 <rkumar113@sapient.com>
2018-06-12 16:54:13 +0530

    Update Http_code_status.sh

--- jenkins/scripts/Http_code_status.sh
+++ jenkins/scripts/Http_code_status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:3000/)
+response=$(curl -s -o /dev/null -w "%{http_code}\n" http://10.203.46.34:3000/)
 if [ "$response" != "200" ]
 then
  exit 1
